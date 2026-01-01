86dc38351a9ea66bf3520225507fa68e25194baa
Basia <barbarawojciechowskaa@gmail.com>
2026-01-01 20:07:08 +0100

    half of the menus is done

--- MVVMFirma/ViewModels/MainWindowViewModel.cs
+++ MVVMFirma/ViewModels/MainWindowViewModel.cs
@@ -1,25 +1,288 @@
+using MVVMFirma.Helper;
+using MVVMFirma.Views;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Collections.ObjectModel;
-using MVVMFirma.Helper;
-using System.Diagnostics;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using System.Windows.Data;
-using MVVMFirma.Views;
+using System.Windows.Input;
 
 namespace MVVMFirma.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        #region FirstAdditionalFeature
+
+        #region Fields1
+        // Contracts Menu
+        private ICommand _AddCommand;
+        private ICommand _ShowCommand;
+        private ICommand _ShowPawnLoanOverviewCommand;
+        private ICommand _ShowInterestRatesCommand;
+        private ICommand _AddPurchaseContractCommand;
+        private ICommand _ShowPurchaseContractCommand;
+        private ICommand _AddPawnLoanCommand;
+        private ICommand _ShowPawnLoanCommand;
+        private ICommand _AddClientsCommand;
+        private ICommand _ShowClientsCommand;
+
+
+        // Network Menu
+        private ICommand _ShowBranchesCommand;
+        private ICommand _AddBranchesCommand;
+        private ICommand _ShowEmployeesCommand;
+        private ICommand _AddEmployeesCommand;
+        private ICommand _ShowEmployeesShiftsCommand;
+
+        #endregion
+
+        #region Commands1
+        // Contracts Menu
+        public ICommand AddCommand
+        {
+            get
+            {
+                if (_AddCommand == null)
+                {
+                    _AddCommand = new BaseCommand(() => Add());
+                }
+                return _AddCommand;
+            }
+        }
+        public ICommand ShowCommand
+        {
+            get
+            {
+                if (_ShowCommand == null)
+                {
+                    _ShowCommand = new BaseCommand(() => Show());
+                }
+                return _ShowCommand;
+            }
+        }
+        public ICommand ShowPawnLoanOverviewCommand
+        {
+            get
+            {
+                if (_ShowPawnLoanOverviewCommand == null)
+                {
+                    _ShowPawnLoanOverviewCommand = new BaseCommand(() => ShowPawnLoanOverview());
+                }
+                return _ShowPawnLoanOverviewCommand;
+            }
+        }
+        public ICommand ShowInterestRatesCommand
+        {
+            get
+            {
+                if (_ShowInterestRatesCommand == null)
+                {
+                    _ShowInterestRatesCommand = new BaseCommand(() => ShowInterestRates());
+                }
+                return _ShowInterestRatesCommand;
+            }
+        }
+        public ICommand AddPurchaseContractCommand
+        {
+            get
+            {
+                if (_AddPurchaseContractCommand == null)
+                {
+                    _AddPurchaseContractCommand = new BaseCommand(() => AddPurchaseContract());
+                }
+                return _AddPurchaseContractCommand;
+            }
+        }
+        public ICommand ShowPurchaseContractCommand
+        {
+            get
+            {
+                if (_ShowPurchaseContractCommand == null)
+                {
+                    _ShowPurchaseContractCommand = new BaseCommand(() => ShowPurchaseContract());
+                }
+                return _ShowPurchaseContractCommand;
+            }
+        }
+        public ICommand AddPawnLoanCommand
+        {
+            get
+            {
+                if (_AddPawnLoanCommand == null)
+                {
+                    _AddPawnLoanCommand = new BaseCommand(() => AddPawnLoan());
+                }
+                return _AddPawnLoanCommand;
+            }
+        }
+        public ICommand ShowPawnLoanCommand
+        {
+            get
+            {
+                if (_ShowPawnLoanCommand == null)
+                {
+                    _ShowPawnLoanCommand = new BaseCommand(() => ShowPawnLoan());
+                }
+                return _ShowPawnLoanCommand;
+            }
+        }
+        public ICommand AddClientsCommand
+        {
+            get
+            {
+                if (_AddClientsCommand == null)
+                {
+                    _AddClientsCommand = new BaseCommand(() => AddClients());
+                }
+                return _AddClientsCommand;
+            }
+        }
+        public ICommand ShowClientsCommand
+        {
+            get
+            {
+                if (_ShowClientsCommand == null)
+                {
+                    _ShowClientsCommand = new BaseCommand(() => ShowClients());
+                }
+                return _ShowClientsCommand;
+            }
+        }
+        // Network Menu
+        public ICommand ShowBranchesCommand
+        {
+            get
+            {
+                if (_ShowBranchesCommand == null)
+                {
+                    _ShowBranchesCommand = new BaseCommand(() => ShowBranches());
+                }
+                return _ShowBranchesCommand;
+            }
+        }
+        public ICommand AddBranchesCommand
+        {
+            get
+            {
+                if (_AddBranchesCommand == null)
+                {
+                    _AddBranchesCommand = new BaseCommand(() => AddBranches());
+                }
+                return _AddBranchesCommand;
+            }
+        }
+        public ICommand ShowEmployeesCommand
+        {
+            get
+            {
+                if (_ShowEmployeesCommand == null)
+                {
+                    _ShowEmployeesCommand = new BaseCommand(() => ShowEmployees());
+                }
+                return _ShowEmployeesCommand;
+            }
+        }
+        public ICommand AddEmployeesCommand
+        {
+            get
+            {
+                if (_AddEmployeesCommand == null)
+                {
+                    _AddEmployeesCommand = new BaseCommand(() => AddEmployees());
+                }
+                return _AddEmployeesCommand;
+            }
+        }
+        public ICommand ShowEmployeesShiftsCommand
+        {
+            get
+            {
+                if (_ShowEmployeesShiftsCommand == null)
+                {
+                    _ShowEmployeesShiftsCommand = new BaseCommand(() => ShowEmployeesShifts());
+                }
+                return _ShowEmployeesShiftsCommand;
+            }
+        }
+        #endregion
+        #region Helper1
+        // Contracts Menu 
+        private void Add()
+        {
+            this.CreateView(new NewBranchViewModel());
+        }
+        private void Show()
+        {
+            this.ShowAllView<AllBranchesViewModel>();
+        }
+        private void ShowPawnLoanOverview()
+        {
+            this.CreateView(new PawnLoansRaportViewModel());
+          }
+        private void ShowInterestRates()
+        {
+            this.ShowAllView<AllInterestRatesViewModel>();
+        }
+        private void AddPurchaseContract()
+        {
+            this.CreateView(new NewPurchaseContractViewModel());
+        }
+        private void ShowPurchaseContract()
+        {
+            this.ShowAllView<AllPurchaseContractsViewModel>();
+        }
+        private void AddPawnLoan()
+        {
+            this.CreateView(new NewPawnLoanViewModel());
+        }
+        private void ShowPawnLoan()
+        {
+            this.ShowAllView<AllPawnLoansViewModel>();
+        }
+        private void AddClients()
+        {
+            this.CreateView(new NewClientViewModel());
+        }
+        private void ShowClients()
+        {
+            this.ShowAllView<AllClientsViewModel>();
+        }
+        // Network Menu
+        private void ShowBranches()
+        {
+            this.ShowAllView<AllBranchesViewModel>();
+        }
+        private void AddBranches()
+        {
+            this.CreateView(new NewBranchViewModel());
+        }
+        private void ShowEmployees()
+        {
+            this.ShowAllView<AllEmployeesViewModel>();
+        }
+        private void AddEmployees()
+        {
+            this.CreateView(new NewEmployeeViewModel());
+        }
+        private void ShowEmployeesShifts()
+        {
+            this.ShowAllView<AllEmployeeShiftsViewModel>();
+        }
+        #endregion
+        #endregion
+
         #region Fields
         private ReadOnlyCollection<CommandViewModel> _Commands;
         private ObservableCollection<WorkspaceViewModel> _Workspaces;
         #endregion
 
         #region Commands
+
+
         public ReadOnlyCollection<CommandViewModel> Commands
         {
             get
@@ -32,6 +295,7 @@ namespace MVVMFirma.ViewModels
                 return _Commands;
             }
         }
+
         private List<CommandViewModel> CreateCommands()
         {
             return new List<CommandViewModel>
@@ -56,7 +320,9 @@ namespace MVVMFirma.ViewModels
                 new CommandViewModel(
                     "-- BEZ FK -- ",
                     new BaseCommand(() => this.CreateView(null))),
+                 
 
+                // proba z branches 
                 new CommandViewModel(
                     "Branches",
                     new BaseCommand(() => this.ShowAllView<AllBranchesViewModel>())),
@@ -219,11 +485,11 @@ namespace MVVMFirma.ViewModels
         private void OnWorkspaceRequestClose(object sender, EventArgs e)
         {
             WorkspaceViewModel workspace = sender as WorkspaceViewModel;
-            //workspace.Dispos();
+            //workspace.Dispose();
             this.Workspaces.Remove(workspace);
         }
 
-        #endregion // Workspaces
+        #endregion 
 
         #region Private Helpers
         private void CreateView(WorkspaceViewModel workspace)
